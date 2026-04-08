68e8f8767ee08b06f5e5432c438b9651440d32b3
Pamella-alvesmarques <pamella.psy@gmail.com>
2026-04-08 18:41:26 -0300

    Add basic login functionality

---
+++ login.py
@@ -0,0 +1,10 @@
+print ("Login")
+print ("Nome do usuário")
+usuario = input ("Digite o usuário    ")
+senha = input ("Digite a senha    ")
+if    usuario == "admin" and senha == "1234":
+    print("Acesso permitido")
+else:
+    print("Usuário ou senha incorretos")
+
+
