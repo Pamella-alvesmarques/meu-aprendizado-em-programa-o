e447c0832aca6c86fdc48a4105c8c556b79d3f33
Pamella-alvesmarques <pamella.psy@gmail.com>
2026-04-08 18:21:10 -0300

    Refactor calculator to loop for multiple operations

--- calculadora.py
+++ calculadora.py
@@ -1,23 +1,38 @@
 print("=== CALCULADORA ===")
 
-num1 = float(input("Digite o primeiro número: "))
-num2 = float(input("Digite o segundo número: "))
+while True:
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
 
-print("Escolha a operação:")
-print("1 - Soma")
-print("2 - Subtração")
-print("3 - Multiplicação")
-print("4 - Divisão")
+    print("Escolha a operação:")
+    print("1 - Soma")
+    print("2 - Subtração")
+    print("3 - Multiplicação")
+    print("4 - Divisão")
+    print("5 - Sair")
+    print("6 - Potêcia")
 
-opcao = input("Digite a opção: ")
+    opcao = input("Digite a opção: ")
 
-if opcao == "1":
-    print("Resultado:", num1 + num2)
-elif opcao == "2":
-    print("Resultado:", num1 - num2)
-elif opcao == "3":
-    print("Resultado:", num1 * num2)
-elif opcao == "4":
-    print("Resultado:", num1 / num2)
-else:
-    print("Opção inválida")
+    if opcao == "1":
+        print("Resultado:", num1 + num2)
+
+    elif opcao == "2":
+        print("Resultado:", num1 - num2)
+
+    elif opcao == "3":
+        print("Resultado:", num1 * num2)
+
+    elif opcao == "4":
+        if num2 == 0:
+            print("Erro: divisão por zero!")
+        else:
+            print("Resultado:", num1 / num2)
+
+    elif opcao == "5":
+        print("Saindo...")
+        break
+    elif opcao == "6":
+        print("Resultado:", num1 ** num2)
+    else:
+        print("Opção inválida")
